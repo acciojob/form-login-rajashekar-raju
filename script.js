035a99870c97cf43a20af4c393f0f7754006f7d3
rajashekar-raju <96567852+rajashekar-raju@users.noreply.github.com>
2024-01-19 15:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+
+
+const form = document.getElementById("form1")
 function getFormvalue() {
     //Write your code here
-
+    const fname = form.fname.value;
+    const lname = form.lname.value;
+	alert(fname+" "+lname);
 }
